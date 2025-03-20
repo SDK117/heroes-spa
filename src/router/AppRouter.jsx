127f7e8ba73121434133784e24dd82bb83c1c339
UlisesHdz <ulisesdejesusk9999@gmail.com>
2025-03-20 10:18:16 -0600

    Rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,26 @@
-import {  Route, Routes } from "react-router-dom"; // Importación correcta
+import { Route, Routes } from "react-router-dom"; // Importación correcta
 import { LoginPage } from "../auth/pages/LoginPage";
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
   return (
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroesRoutes />} />
-      </Routes>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+
+
+
+      //rutas privadas
+      <Route
+        path="/*"
+        element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+        }
+      />
+      {/* <Route path="/*" element={<HeroesRoutes />} /> */}
+       
+    </Routes>
   );
 };
